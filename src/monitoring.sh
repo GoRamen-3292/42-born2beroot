b0f19598cb60a0df700e52cef19be4210ab616cc
ktomita <ktomita@student.42tokyo.jp>
2026-08-05 14:13:43 +0900

    add: メモリ使用量について追加

--- src/monitoring.sh
+++ src/monitoring.sh
@@ -4,21 +4,45 @@ printf "#Architecture: "
 uname --all
 
 printf "#Physical CPU: "
+# Get the largest number of the physical id
 grep -F 'physical id' /proc/cpuinfo | sort -u | wc -l
 
 printf "#vCPU: "
-grep -F 'physical id' /proc/cpuinfo | sort -u | wc -l
+echo
 
-# uname --kernel-name | head -c -1
-# printf " "
-# uname --nodename | head -c -1
-# printf " "
-# uname --kernel-version | head -c -1
-# printf " "
-# uname --kernel-release | head -c -1
-# printf " "
-# uname --hardware-platform | head -c -1
-# printf " "
-# uname --processor | head -c -1
-# printf " "
-# uname -- | head -c -1
+printf "#Memory Usage: "
+
+free_result_h=`free -h | grep Mem:`
+available_h=`echo $free_result_h | awk '{print $7}'`
+total_h=`echo $free_result_h | awk '{print $2}'`
+
+free_result=`free | grep Mem:`
+available=`echo $free_result | awk '{print $7}'`
+total=`echo $free_result | awk '{print $2}'`
+free_percent=$(($available * 100 / $total))
+
+echo "$available_h / $total_h ($free_percent %)"
+
+printf "#Disk Usage: "
+echo
+
+printf "#CPU load: "
+echo
+
+printf "#Last boot: "
+echo
+
+printf "#LVM Use: "
+echo
+
+printf "#TCP Connections: "
+echo
+
+printf "#User log: "
+echo
+
+printf "#Network: "
+echo
+
+printf "#Sudo: "
+echo
